c15fb695719996b7ab739ff851d6a3dd81ce8e29
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-03-02 19:45:45 +0500

    Create Task 2.js

---
+++ TASKS/Task 2.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 02</title>
+</head>
+<body>
+    <script>
+
+        var num1 = parseInt(prompt("Enter first number"))
+        var num2 = parseInt(prompt("Enter second number"))
+
+        if (num1 == num2) {
+           alert("EQUAL")
+        }
+            
+        else  if (num1 > num2) {
+            alert("GREATER")
+        } 
+            
+         else{
+            alert("LESSER")
+        } 
+    </script>
+</body>
+</html>
