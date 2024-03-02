4fff26306adbb58d947b358942f8ece26d11107a
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-03-02 19:46:04 +0500

    Create Task 3.js

---
+++ TASKS/Task 3.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 03</title>
+</head>
+<body>
+    <script>
+        var  num1 = parseInt(prompt("Enter first number")); 
+        var num2 = parseInt(prompt("Enter second number"));
+        var num3 = parseInt(prompt("Enter third number"));
+        
+        if (num1 < 0 || num2 < 0 || num3 < 0) {
+            alert("-")
+        } else{
+            alert("+")
+        }
+    </script>
+</body>
+</html>
