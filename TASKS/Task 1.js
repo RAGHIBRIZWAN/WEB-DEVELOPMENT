3ddc0816510ee15980d009721552065e7ddc3418
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-03-02 19:45:25 +0500

    Create Task 1.js

---
+++ TASKS/Task 1.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 01</title>
+</head>
+<body>
+    <script>
+        var age = parseInt(prompt("Enter Your age"));
+       
+        if (age >= 18) {
+            alert("QUALIFIED");
+        } else {
+            alert("FAILED");
+        }
+    </script>
+</body>
+</html>
